0127662a7ae7014b9850004ba0891a62ea1f8f63
herczegAkos25 <akos.herczeg25@gmail.com>
2026-01-16 08:22:21 +0100

    Seasons updated

--- Program.cs
+++ Program.cs
@@ -81,10 +81,10 @@
             public string Season(int month)
             {
                 // TODO : Return season based on month value
-                if (month == 1 || month == 2 || month == 12) return "winter";
-                else if (month == 3 || month == 4 || month == 5) return "spring";
-                else if (month == 6 || month == 7 || month == 8) return "summer";
-                else if (month == 9 ||month == 10 || month ==11) return "autumn";
+                if (month == 1 || month == 2 || month == 3) return "winter";
+                else if (month == 4 || month == 5 || month == 6) return "spring";
+                else if (month == 7 || month == 8 || month == 9) return "summer";
+                else if (month == 10 ||month == 11 || month ==12) return "fall";
                 else throw new NotImplementedException();
             }
             /// &lt;summary&gt;
